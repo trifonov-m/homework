edadad2dc6dccbc65d5f6dcca44b88da3d0425d0
Mikhail Trifonov <mtrifonov@list.ru>
2020-09-07 22:33:06 +0300

    Первый раз добавил commit

---
+++ lesson1/dtypes.py
@@ -0,0 +1,4 @@
+#print(2 / 2)
+#name = input('Введите ваше имя: ')
+#print(f'Привет, {name}! Как дела?')
+print(bool(0))
---
+++ lesson1/info.py
@@ -0,0 +1,12 @@
+# Тут задания из слайдов "Переменные"
+
+# Вместо hello.py
+name = 'Mikhail'
+print(name)
+
+# А это сам info.py
+user_info = {
+    'first_name' : 'Mikhail',
+    'second_name' : 'Trifonov'
+}
+print(user_info['first_name'])
