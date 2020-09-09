0b8525a72c191a0d6635ddde912559988ea15094
Mikhail <mtrifonov@list.ru>
2020-09-09 17:03:28 +0300

    Тестовая версия бота для ephem

--- learn-homework-1/8_ephem_bot.py
+++ learn-homework-1/8_ephem_bot.py
@@ -13,46 +13,66 @@
 
 """
 import logging
-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import datetime
+import settings
+import ephem
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log'
-)
+# Делаем вывод логов в bot.log
+logging.basicConfig(handlers=[logging.FileHandler('bot.log', 'w', 'utf-8')], level=logging.INFO)
 
-
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn', 
-        'password': 'python'
-    }
-}
+# Настройки прокси
+PROXY = {'proxy_url': settings.PROXY_URL,
+    'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
 
 
-def greet_user(bot, update):
-    text = 'Вызван /start'
+def greet_user(update, context):
+    print('Вызван /start')
+    # update.message.reply_text - ответ в чате телеграм
+    update.message.reply_text('Привет, пользователь! Ты вызвал команду /start')
+
+# Эхо сообщения
+def talk_to_me(update, context):
+    text = update.message.text
     print(text)
     update.message.reply_text(text)
 
 
-def talk_to_me(bot, update):
-    user_text = update.message.text 
+def planet_greet(update, context):
+    text = 'Вызван /planet'
+    print(text)
+    update.message.reply_text(text)
+
+
+def planet_sozv(update, context):
+    user_text = update.message.text
     print(user_text)
-    update.message.reply_text(user_text)
- 
+    d = datetime.datetime.now()
+    date = d
+#    date = d.strftime("%Y/%m/%d")
+    if user_text == 'Jupiter':
+        planet = ephem.Jupiter(date) 
+    update.message.reply_text(ephem.constellation(planet))
+    
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
-    
+    # Создаем бота и передаем ему ключ для авторизации на серверах Telegram
+    mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
+    # Работа с диспетчером
     dp = mybot.dispatcher
+    # Регистрация обработчика у диспетчера. Greet_user - функция, которая будет выполняться при start.
     dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
-    
+    # Добавляю команду на /planet
+    dp.add_handler(CommandHandler("planet", planet_greet))
+    # Добавляем обработчик сообщений, принимает только текстовые. talk_to_me - вызываемая функция
+ #   dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(MessageHandler(Filters.text, planet_sozv))
+    # Вывод в лог инфы о том, что бот стартовал
+    logging.info("Бот стартовал")
+    # Регулярные частые обращения за обновлениями. (Бот ходит в телегу за сообщениями)
     mybot.start_polling()
+    # Бесконечный цикл бота, пока принудительно не остановишь
     mybot.idle()
-       
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
