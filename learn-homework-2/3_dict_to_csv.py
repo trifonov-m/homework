989e2855d404a8182f7d76fa3cc266800b6a872d
Mikhail <mtrifonov@list.ru>
2020-09-16 14:40:24 +0300

    Сделал csv

--- learn-homework-2/3_dict_to_csv.py
+++ learn-homework-2/3_dict_to_csv.py
@@ -9,13 +9,26 @@
 2. Запишите содержимое списка словарей в файл в формате csv
 
 """
+import csv
+
 
 def main():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    task_list = [
+        {'name': 'Маша', 'age': 25, 'job': 'Scientist'},
+        {'name': 'Вася', 'age': 8, 'job': 'Programmer'},
+        {'name': 'Эдуард', 'age': 48, 'job': 'Big boss'},
+        {'name': 'Михаил', 'age': 25, 'job': 'Future Junior'}
+    ]
+    fields = list(task_list[0].keys())
+    with open('task3.csv', 'w', encoding='utf-8') as task3:
+        writer = csv.DictWriter(task3, fields, delimiter=';')
+        writer.writeheader()
+        writer.writerows(task_list)
+
 
 if __name__ == "__main__":
     main()
